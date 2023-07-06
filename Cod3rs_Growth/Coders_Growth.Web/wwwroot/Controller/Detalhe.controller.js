a955856acbd2f1df78b36e59d89295fae49a4e26
Vinipcsl <viniciuspcsl@gmail.com>
2023-07-06 10:33:49 -0300

    feito a alteração colocando as strings em const, usando i18n para as mensagens de tela e configurado o .gitignore

--- Cod3rs_Growth/Coders_Growth.Web/wwwroot/Controller/Detalhe.controller.js
+++ Cod3rs_Growth/Coders_Growth.Web/wwwroot/Controller/Detalhe.controller.js
@@ -4,12 +4,17 @@ sap.ui.define([
 ], function (Controller, JSONModel) {	
 	"use strict";
 	const uri="https://localhost:59606/api/celular/";
+	const rotaDetalhe="sap.ui.demo.viniCelulares.controller.Detalhe";
+	const lista="listaDeCelulares";
+	
 
-	return Controller.extend("sap.ui.demo.viniCelulares.controller.Detalhe", {
+	return Controller.extend(rotaDetalhe, {
 		
 		onInit: function () {
+			const detalhe="detalhe";
+
 			var oRouter = this.getOwnerComponent().getRouter();
-			oRouter.getRoute("detalhe").attachPatternMatched(this._aoCoincidirRota, this);
+			oRouter.getRoute(detalhe).attachPatternMatched(this._aoCoincidirRota, this);
 		},
 
 		_aoCoincidirRota: function (oEvent) {
@@ -19,17 +24,19 @@ sap.ui.define([
 
 		aoClicarEmVoltar: function () {			
 			let oRouter = this.getOwnerComponent().getRouter();
-            oRouter.navTo("listaDeCelulares", {}, true);
+            oRouter.navTo(lista, {}, true);
 		},
 
 		_detalhes : function (id){
+			const celular="celular";
+
             let tela = this.getView();
             fetch(`${uri}${id}`)
                .then(function(response){
                   return response.json();
                })
                .then(function (data){
-                  tela.setModel(new JSONModel(data),"celular")
+                  tela.setModel(new JSONModel(data),celular)
                })
                .catch(function (error){
                   console.error(error);
--- Cod3rs_Growth/Coders_Growth.Web/wwwroot/Controller/ListaDeCelulares.controller.js
+++ Cod3rs_Growth/Coders_Growth.Web/wwwroot/Controller/ListaDeCelulares.controller.js
@@ -9,8 +9,12 @@ sap.ui.define([
 	"use strict";
 
 	const uri="https://localhost:59606/api/celular/";
-
-	return Controller.extend("sap.ui.demo.viniCelulares.controller.ListaDeCelulares", {
+	const caminhoLista = "sap.ui.demo.viniCelulares.controller.ListaDeCelulares";
+	const celulares = "celulares";
+	const celularesDaLista= "celularesDaLista";
+	const items= "items";
+	const filtoMarca="marca";
+	return Controller.extend(caminhoLista, {
 
 		onInit:function() {
 			let tela = this.getView();
@@ -19,7 +23,7 @@ sap.ui.define([
 				  return response.json();
 			   })
 			   .then(data => {
-				  tela.setModel(new JSONModel(data),"celulares");
+				  tela.setModel(new JSONModel(data),celulares);
 			   })
 			   .catch(function (error){
 				  console.error(error);
@@ -31,14 +35,14 @@ sap.ui.define([
 				aFilter = [];
 				if (sQuery)
 				{
-					aFilter.push(new Filter("marca", FilterOperator.Contains, sQuery));
+					aFilter.push(new Filter(filtoMarca, FilterOperator.Contains, sQuery));
 				}
-				var oTable = this.byId("celularesDaLista");
-				var oBinding = oTable.getBinding("items");
+				var oTable = this.byId(celularesDaLista);
+				var oBinding = oTable.getBinding(items);
 				oBinding.filter(aFilter);			
 		 },
 		 aoClicarNaLinha: function (evento) {
-			let id = evento.getSource().getBindingContext("celulares").getObject().id
+			let id = evento.getSource().getBindingContext(celulares).getObject().id
    
 			let oRouter = this.getOwnerComponent().getRouter()
 			oRouter.navTo("detalhe", {id})

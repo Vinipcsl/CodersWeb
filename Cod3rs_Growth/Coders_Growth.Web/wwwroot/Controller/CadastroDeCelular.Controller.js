cdbe161d913763fd839c496e349d69dd6e4d039e
Vinipcsl <viniciuspcsl@gmail.com>
2023-07-07 10:25:57 -0300

    ..

--- Cod3rs_Growth/Coders_Growth.Web/wwwroot/Controller/CadastroDeCelular.Controller.js
+++ Cod3rs_Growth/Coders_Growth.Web/wwwroot/Controller/CadastroDeCelular.Controller.js
@@ -6,14 +6,61 @@ sap.ui.define([
 	const uri="https://localhost:59606/api/celular/";
 	const caminhoControllerCadastroDeCelular="sap.ui.demo.viniCelulares.controller.CadastroDeCelular";
 	const lista="listaDeCelulares";
+	const modeloCelular = "celulares";
 	
 
 	return Controller.extend(caminhoControllerCadastroDeCelular, {	
+
+		onInit : function() {
+			let oRouter = this.getOwnerComponent().getRouter();
+			oRouter.attachRoutePatternMatched(this._aoCoincidirRota, this);
+		},
+
+		_aoCoincidirRota: function()
+		{
+			this.setarModeloCelular();
+		},
+		setarModeloCelular: function()
+		{
+			const stringVazia = "";
+			let celular= {
+				marca: stringVazia,
+				modelo:stringVazia,
+				cor: stringVazia,
+				memoria: stringVazia,
+				anoFabricado: stringVazia
+			}
+			this.getView().setModel(new JSONModel(celular),modeloCelular);
+		},
+		aoClicarEmSalvar: function()
+		{
+			const celular = this.getView()
+			.getModel(modeloCelular)
+			.getData();
+			this._salvarCelular(celular);
+		},
+		_salvarCelular: function(celular)
+		{
+			rotaDetalhe = "detalhe";
+			fetch(uri,{
+				method:"POST",
+				headers:{
+					"Content-Type": "application/json",
+				},
+				body:JSON.stringify(celular)
+			})
+			.then((response)=> response.json())
+			.then(anoFabricado => this._navegar(rotaDetalhe))
+		},
+
+		_navegar: function(lista){
+            let oRouter = this.getOwnerComponent().getRouter();
+            oRouter.navTo(lista);
+        },
     
         aoClicarEmVoltar: function () {
-			this._limparTela()
 			let oRouter = this.getOwnerComponent().getRouter();
-            oRouter.navTo(lista, {}, true);
+            this._navegar(lista);
 		},
     
         });
